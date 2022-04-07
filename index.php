c5659c22aa89ffa88caf596659a9a01fd6c61152
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-04-07 17:54:15 +0200

    $_GET the user word

--- index.php
+++ index.php
@@ -9,11 +9,12 @@
 <body>
     <?php
         $testo = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque ipsum quisquam cupiditate delectus, eius ullam nesciunt hic molestiae fuga totam facere fugiat, distinctio modi ea, pariatur itaque molestias quis.';
-    ?>
+        
+        ?>
     
     <p>Questo è il testo: <?php echo $testo;?></p>
-    <p>Quante parole sono? <?php echo strlen($testo);?></p>
-    
+    <p>Quanti caratteri sono? <?php echo strlen($testo);?></p>
+
     <form action="index.php" method="get">
         Bad Word: <input type="text" name="badWord"><br>
         <input type="submit">
@@ -21,5 +22,7 @@
     <p>
         la tua parola da censurare è: <?php echo $_GET["badWord"]; ?>
     </p> 
+
+    
 </body>
 </html>
