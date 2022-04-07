6352f1f24924ab12eff65f6db576c2303f205b8b
LauraMargherita25 <96023276+LauraMargherita25@users.noreply.github.com>
2022-04-07 17:24:09 +0200

    Not working

--- index.php
+++ index.php
@@ -9,10 +9,17 @@
 <body>
     <?php
         $testo = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque ipsum quisquam cupiditate delectus, eius ullam nesciunt hic molestiae fuga totam facere fugiat, distinctio modi ea, pariatur itaque molestias quis.';
-        $arrTesto = explode(" ", $testo);
     ?>
     
     <p>Questo è il testo: <?php echo $testo;?></p>
     <p>Quante parole sono? <?php echo strlen($testo);?></p>
+    
+    <form action="index.php" method="get">
+        Bad Word: <input type="text" name="badWord"><br>
+        <input type="submit">
+    </form>
+    <p>
+        la tua parola da censurare è: <?php echo $_GET["badWord"]; ?>
+    </p> 
 </body>
 </html>
